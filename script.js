c3cae41bc04003b65fa4f124535ff17c7d8f83ee
Joseph Adamo <josephaadamo67@gmail.com>
2022-03-09 11:49:34 -0600

    Created Operate Function

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ const multiply = function(num1, num2) {
 const divide = function(num1, num2) {
     return num1 / num2;
 };
+
+const operate = function(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
